fbef52a20386e23584fa85b9a6c8afa12e6650fe
Poostoi <oleg.nour@bk.ru>
2022-12-20 23:14:17 +0200

    Change function

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-numbers = input("Введите числа разделённые пробелом")
-
-
 def high_and_low(numbers):
-    min = int(numbers[0])
-    max = int(numbers[0])
-    number = "";
-    for i in range(len(numbers)):
-        if numbers[i] == '':
-            pass
-        elif numbers[i] == " ":
-            number = ""
-            continue
-        elif numbers[i + 1] != " ":
-            number += numbers[i]
-            continue
-        number += numbers[i]
-        if min > int(number):
-            min = int(number)
-        else:
-            max = int(number)
+    arr_str = numbers.split()
+    min_el = int(arr_str[0])
+    max_el = int(arr_str[0])
+    for element in arr_str:
+        if min_el > int(element):
+            min_el = int(element)
+        if max_el < int(element):
+            max_el = int(element)
 
-    return f"{max} {min}"
+    return f"{max_el} {min_el}"
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
+
+
