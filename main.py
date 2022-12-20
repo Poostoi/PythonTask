8c542737a1b6a115afe118df572d09cdb8cfd8d0
Poostoi <oleg.nour@bk.ru>
2022-12-20 14:00:06 +0200

    fix

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 numbers = input("Введите числа разделённые пробелом")
-min = int(numbers[0])
-max = int(numbers[0])
-for number in numbers:
-    if number == " ":
-        pass
-    elif min > int(number):
-        min = int(number)
-    elif max < int(number):
-        max = int(number)
 
-print(f"{min} {max}")
+
+def high_and_low(numbers):
+    min = int(numbers[0])
+    max = int(numbers[0])
+    number = "";
+    for i in range(len(numbers)):
+        if numbers[i] == '':
+            pass
+        elif numbers[i] == " ":
+            number = ""
+            continue
+        elif numbers[i + 1] != " ":
+            number += numbers[i]
+            continue
+        number += numbers[i]
+        if min > int(number):
+            min = int(number)
+        else:
+            max = int(number)
+
+    return f"{max} {min}"
